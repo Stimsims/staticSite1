beedbf7a1166469c230307615f5dd3ba0646479f
illulli <illu.development@gmail.com>
2018-06-11 15:48:30 +1000

    basic website functioning

---
+++ static.config.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+ import posts from './posts/index.js';
+
+export default {
+  getSiteData: () => ({
+    title: 'React Static',
+  }),
+  getRoutes: async () => {
+    //const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+   // let posts = file;
+    return [
+      {
+        path: '/',
+        component: 'src/containers/Blog',
+        getData: () => ({
+          posts,
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          component: 'src/containers/Post',
+          getData: () => ({
+            post,
+          }),
+        })),
+      },
+      {
+        path: '/about',
+        component: 'src/containers/About',
+      },
+      {
+        is404: true,
+        component: 'src/containers/404',
+      },
+    ]
+  },
+}
